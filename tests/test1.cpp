646eb21c268997edebcdc4821b0ed98ad28fc7df
TarasKoval <antares13.1998@gmail.com>
2018-08-05 20:45:21 +0300

    one test uses code under the coverage check

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -2,6 +2,8 @@
 #include "gtest/gtest.h"
 #include "functions.h"
 
-TEST(Test, TEST_ONE) {
-    EXPECT_EQ(1, 1);
+TEST(mathExpectationTwoParameters, outOfBoundsGap) {
+    const std::vector<ulong> inputData = {343, 222, 2335};
+    auto result = mathExpectation(inputData, 3);
+    EXPECT_EQ(1450, result);
 }
